868241a33999349ac68fe76f90d72af0da309935
Christoph Kiefer <christophkiefer@web.de>
2017-05-30 18:19:18 +0200

    Rename files

---
+++ installPackages/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+
+namespace installPackages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            bool isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (isAdmin)
+            {
+                r_calls Rinstance;
+                Rinstance = new r_calls();
+                Rinstance.initREngine();
+                Rinstance.installPackages();
+            }
+            else
+            {
+                
+            }
+
+            
+
+        }
+    }
+}
---
+++ installPackages/r_calls.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RDotNet;
+using RDotNet.Devices;
+using RDotNet.Dynamic;
+using RDotNet.Internals;
+using RDotNet.NativeLibrary;
+
+
+namespace installPackages
+{
+    class r_calls
+    {
+        REngine engine;
+
+        public void initREngine()
+        {
+            //REngine.SetEnvironmentVariables("R-Portable/App/R-Portable/bin/i386", "R-Portable/App/R-Portable");
+            engine = REngine.GetInstance();
+            engine.Initialize();
+        }
+
+        
+        public void installPackages()
+        {
+            engine.Evaluate("install.packages('packages', repos = 'https://cran.rstudio.com')");
+        }
+
+
+
+    }
+}
